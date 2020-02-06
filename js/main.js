5bdf2a4d5f91e1da0733e154a635c2aa6483a846
Nikolai Leonov <strongardo@gmail.com>
2020-02-06 15:30:19 +0600

    Добавляет вывод карточки с условиями жилья

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var similarAdsQuantity = 8;
-var typeOfHousing = ['palace', 'flat', 'house', 'bungalo'];
+var typesOfHousing = ['palace', 'flat', 'house', 'bungalo'];
 var checks = ['12:00', '13:00', '14:00'];
 var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
@@ -18,6 +18,7 @@ var maxRoomsQuantity = 5;
 var minGuestsQuantity = 1;
 var maxGuestsQuantity = 5;
 var map = document.querySelector('.map__pins');
+var mapContainer = map.querySelector('.map__filters-container');
 
 var getRandomElement = function (arr) {
   return arr[Math.floor(Math.random() * (arr.length))];
@@ -29,7 +30,7 @@ function getRandomNumber(min, max) {
 
 function createRandomArray(arr) {
   var randomArr = [];
-  for (var i = 0; i < getRandomNumber(1, arr.length); i++) {
+  for (var i = 0; i < getRandomNumber(1, arr.length + 1); i++) {
     randomArr.push(arr[i]);
   }
   return randomArr;
@@ -52,7 +53,7 @@ var createSimilarAds = function (quantity) {
       title: 'title',
       address: '600, 350',
       price: getRandomNumber(minPrice, maxPrice),
-      type: getRandomElement(typeOfHousing),
+      type: getRandomElement(typesOfHousing),
       rooms: getRandomNumber(minRoomsQuantity, maxRoomsQuantity),
       guests: getRandomNumber(minGuestsQuantity, maxGuestsQuantity),
       checkin: getRandomElement(checks),
@@ -91,5 +92,67 @@ var renderAds = function (ads, block) {
   block.appendChild(fragment);
 };
 
-renderAds(createSimilarAds(similarAdsQuantity), map);
+var createCardElement = function (obj) {
+  var template = document.querySelector('#card').content.querySelector('.map__card');
+  var card = template.cloneNode(true);
+  var popupTitle = card.querySelector('.popup__title');
+  var popupAddress = card.querySelector('.popup__text--address');
+  var popupPrice = card.querySelector('.popup__text--price');
+  var popupType = card.querySelector('.popup__type');
+  var popupCapacity = card.querySelector('.popup__text--capacity');
+  var popupTime = card.querySelector('.popup__text--time');
+  var popupFeatures = card.querySelector('.popup__features');
+  var popupDescription = card.querySelector('.popup__description');
+  var popupPhotos = card.querySelector('.popup__photos');
+  var popupAvatar = card.querySelector('.popup__avatar');
 
+  popupTitle.textContent = obj.offer.title;
+  popupAddress.textContent = obj.offer.address;
+  popupPrice.textContent = obj.offer.price + '₽/ночь';
+
+  if (obj.offer.type === 'flat') {
+    popupType.textContent = 'Квартира';
+  } else if (obj.offer.type === 'bungalo') {
+    popupType.textContent = 'Бунгало';
+  } else if (obj.offer.type === 'house') {
+    popupType.textContent = 'Дом';
+  } else if (obj.offer.type === 'palace') {
+    popupType.textContent = 'Дворец';
+  }
+
+  popupCapacity.textContent = obj.offer.rooms + ' комнаты для ' + obj.offer.guests + ' гостей.';
+  popupTime.textContent = 'Заезд после ' + obj.offer.checkin + ', выезд до ' + obj.offer.checkout + '.';
+
+  for (var i = 0; i < popupFeatures.children.length; i++) {
+    if (i > obj.offer.features.length - 1) {
+      popupFeatures.children[i].style.display = 'none';
+    }
+  }
+
+  popupDescription.textContent = obj.offer.description;
+  popupPhotos.children[0].src = obj.offer.photos[0];
+
+  if (obj.offer.photos.length > 1) {
+    var fragment = document.createDocumentFragment();
+    for (var j = 1; j < obj.offer.photos.length; j++) {
+      var photo = popupPhotos.children[0].cloneNode(true);
+      photo.src = obj.offer.photos[j];
+      fragment.appendChild(photo);
+    }
+    popupPhotos.appendChild(fragment);
+  }
+
+  popupAvatar.src = obj.author.avatar;
+
+  return card;
+};
+
+var renderCards = function (arr, block, beforeBlock) {
+  var card = createCardElement(arr[0]);
+  block.insertBefore(card, beforeBlock);
+};
+
+var housingData = createSimilarAds(similarAdsQuantity);
+
+renderAds(housingData, map);
+renderCards(housingData, map, mapContainer);
